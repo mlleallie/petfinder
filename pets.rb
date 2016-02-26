ddd75a4a0ae5db83006607baaf18f9a17e12ad7e
zolon wilkins <zolonsk8@gmail.com>
2016-02-26 11:18:24 -0600

    started id

--- pets.rb
+++ pets.rb
@@ -2,4 +2,14 @@ require 'httparty'
 require 'pry'
 
 
-response = HTTParty.get()
+random_pet = HTTParty.get("http://api.petfinder.com/pet.getRandom?format=json&key=2fb23a71a91f020950eb1c8c33d73982")
+random_dog = HTTParty.get("http://api.petfinder.com/pet.getRandom?format=json&key=2fb23a71a91f020950eb1c8c33d73982&animal=dog")
+
+
+def open_dog
+
+ system("open #{random_dog} ")
+
+end
+
+open_dog.new
